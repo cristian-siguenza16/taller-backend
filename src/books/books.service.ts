f4062dd6c8c63961a77eee26a29f1574aa6b966a
cristian-siguenza16 <csiguenzavelasquez@gmail.com>
2024-10-23 20:49:43 -0600

    Created services to handle logic of Create,  Read(getOne, getAll), Update, Delete books

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Book } from './entities/book.entity';
+import { Repository } from 'typeorm';
+import { CreateBookDto } from './dtos/create-book.dto';
+import UpdateBookDto from './dtos/update-book.dto';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  constructor(
+    @InjectRepository(Book)
+    private readonly booksRepository: Repository<Book>,
+  ) { }
+
+  findAll() {
+    return this.booksRepository.find();
+  }
+
+  async findOne(id: number) {
+    const record = await this.booksRepository.findOne({
+      where: { id },
+    });
+
+    if (record === null) {
+      throw new NotFoundException(`Book #${id} not found.`);
+    }
+
+    return record;
+  }
+
+  create(newBook: CreateBookDto) {
+    const book = this.booksRepository.create(newBook);
+    return this.booksRepository.save(book);
+  }
+
+  async update(id: number, updateBook: UpdateBookDto) {
+    const book = await this.findOne(id)
+
+    this.booksRepository.merge(book, updateBook);
+
+    return this.booksRepository.save(book);
+  }
+
+  async remove(id: number) {
+    const book = await this.findOne(id);
+    return this.booksRepository.remove(book);
+  }
+}
